cf6166d10bcd8a4b32d5b57befcf0d93aa5446d6
Jitendra <jitujadhav112@gmail.com>
2020-07-09 14:36:05 +0530

    Add New Controller

--- WebApplication1/Controllers/AdminController.cs
+++ WebApplication1/Controllers/AdminController.cs
@@ -1,1 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WebApplication1.Models;
 
+
+namespace WebApplication1.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
